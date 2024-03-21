e4360e2e332edca21d9f6090a67db22bcc3f3317
djessycousto <79341027+djessycousto@users.noreply.github.com>
2024-03-21 12:42:00 +0000

    small fix

--- app.js
+++ app.js
@@ -44,12 +44,12 @@ app.use(express.static("./public"));
 // careful with the endpoint position
 // app.use("/auth", authRouter); // View-related routes
 app.use("/api/v1/auth", authRouter); // API-related routes
-app.use("/blogs", blogRouterView); // View-related routes
+app.use("/", blogRouterView); // View-related routes
 
 //API
 // app.use("/api/v1/auth", authRouter); // API-related routes
 // app.use("/api/v1/blogs", blogRouter); // API routes
-app.use("/dashboard", dashRouter); // API-related routes
+// app.use("/dashboard", dashRouter); // API-related routes
 
 // ///////// old route to be replace ////////////////////////////////////////
 
